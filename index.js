54219919de21f62c2eb67434d756470f5439671e
jadmi <152192404+jadmi@users.noreply.github.com>
2025-05-27 17:00:30 +0200

    changes

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-function carousel() {
-  return {
-    currentIndex: 0,
-    images: [
-      "https://picsum.photos/id/1015/600/300",
-      "https://picsum.photos/id/1016/600/300",
-      "https://picsum.photos/id/1018/600/300",
-    ],
-    next() {
-      this.currentIndex = (this.currentIndex + 1) % this.images.length;
-    },
-    prev() {
-      this.currentIndex =
-        (this.currentIndex - 1 + this.images.length) % this.images.length;
-    },
-  };
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const slider = document.querySelector(".slider-container");
+  const articles = slider.querySelectorAll("article");
+  const leftButton = document.querySelector(".prev");
+  const rightButton = document.querySelector(".next");
+});
