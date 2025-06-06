c049bdd58b502dc8dad410c0b2e2ffb6ae28ed18
jadmi <justin.admiraal@hva.nl>
2025-06-06 14:04:17 +0200

    final versie vanilla - optimized

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 const slider = document.querySelector(".slider-container ul");
+const sliderContainer = document.querySelector(".slider-container");
 const listItems = slider.querySelectorAll("li");
 const leftButton = document.querySelector(".prev");
 const rightButton = document.querySelector(".next");
+const firstItem = listItems[0];
+const lastItem = listItems[listItems.length - 1];
 const gapValue = Math.round(
-  (slider.scrollWidth - listItems[0].clientWidth) / (listItems.length - 1) -
-    listItems[0].clientWidth
+  (slider.scrollWidth - firstItem.clientWidth) / (listItems.length - 1) -
+    firstItem.clientWidth
 );
-
-const extraScrollSpace = 0.1; // 10% item width extra space to scroll
+const observer = new IntersectionObserver(observeCallback, {
+  root: sliderContainer,
+  threshold: 0.9,
+});
 
 leftButton.disabled = true;
-slider.addEventListener("scroll", onScroll);
-rightButton.addEventListener("click", () => handleSliderNavigation("right"));
-leftButton.addEventListener("click", () => handleSliderNavigation("left"));
+rightButton.addEventListener("click", handleSliderNavigation);
+leftButton.addEventListener("click", handleSliderNavigation);
 
 listItems.forEach((card) => {
   card.addEventListener("click", onCardClick);
 });
+
+observer.observe(firstItem);
+observer.observe(lastItem);
 
 function onCardClick(e) {
   const link = e.currentTarget.querySelector("a");
@@ -25,35 +32,26 @@ function onCardClick(e) {
   }
 }
 
-function onScroll() {
-  const itemWidth = listItems[0].offsetWidth + gapValue;
-  const maxScrollLeft = slider.scrollWidth - slider.clientWidth;
-
-  rightButton.disabled = slider.scrollLeft + itemWidth * 0.1 >= maxScrollLeft;
-  leftButton.disabled = slider.scrollLeft <= 0;
-}
-
-function handleSliderNavigation(direction) {
-  const itemWidth = listItems[0].offsetWidth + gapValue;
-  const maxScrollLeft = slider.scrollWidth - slider.clientWidth;
-
-  if (direction === "right") {
+function handleSliderNavigation(e) {
+  const itemWidth = firstItem.offsetWidth + gapValue;
+  if (e.target === rightButton) {
     slider.scrollLeft += itemWidth;
-
-    if (slider.scrollLeft + itemWidth * extraScrollSpace > maxScrollLeft) {
-      rightButton.disabled = true;
-    }
-    leftButton.disabled = false;
-  }
-
-  if (direction === "left") {
+  } else {
     slider.scrollLeft -= itemWidth;
-
-    rightButton.disabled = false;
-    leftButton.disabled = slider.scrollLeft <= 0;
   }
 }
 
+function observeCallback(entries) {
+  entries.forEach((entry) => {
+    if (entry.target === firstItem) {
+      leftButton.disabled = entry.isIntersecting;
+    }
+
+    if (entry.target === lastItem) {
+      rightButton.disabled = entry.isIntersecting;
+    }
+  });
+}
 // TODO: remove floating numbers
 // TODO: functies lostrekken
 // TODO: handle duplicate code
